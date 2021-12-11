2e00d024e6540a89c395b9a04383b47d08be43a0
sukdong-kim <73944038+sukdong-kim@users.noreply.github.com>
2021-12-11 21:28:14 +0900

    dn update

---
+++ ex05_8/src/student/Main.java
@@ -0,0 +1,33 @@
+package student;
+	
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.BorderPane;
+import javafx.fxml.FXMLLoader;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("view/Sample.fxml"));
+			Scene scene = new Scene(root,600,400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			Image image = new Image(getClass().getResource("/images/hoseo.png").toExternalForm());
+			primaryStage.getIcons().add(image);
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("학생 관리");
+
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
